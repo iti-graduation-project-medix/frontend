9edd3c9c4adcf235c3c42f74130dd64801f81917
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-07-23 22:26:18 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance with default config
 const api = axios.create({

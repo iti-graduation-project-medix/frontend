b097635157b6098b5cf606cfd27e2bb0e79875fd
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-07-23 22:27:12 +0300

    Update chatApi.js

--- src/services/chatApi.js
+++ src/services/chatApi.js
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 const API_BASE_URL = `${baseURL}/api/v1`;
 
 export const getChatHistory = async (chatRoomId) => {

7e9be001b1d95a0ea2d19c32394bb5d217d9d297
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-07-24 19:12:44 +0300

    Add upload image button and handle its api

--- src/api/profile/profile.js
+++ src/api/profile/profile.js
@@ -29,3 +29,21 @@ export const getUserDetailsById = async (id, token) => {
     );
   }
 };
+
+export const updateUserProfileImage = async (file, token) => {
+  const formData = new FormData();
+  formData.append('image', file);
+  try {
+    const response = await api.patch('/user/update-image', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || 'Failed to update profile image'
+    );
+  }
+};
